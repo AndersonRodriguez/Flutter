23ed544e829e0ce3b39269842f742d99641aeb20
AndersonRodriguez <anderson.rodri26@gmail.com>
2023-09-14 20:02:45 -0500

    update

--- ejercicio 2.dart
+++ ejercicio 2.dart
@@ -10,4 +10,8 @@
  * }
  * 
  * Transformar este map en un objeto Person por medio del contructor
+ * 
+ * Metodo que imprima el nombre de la persona y una accion
+ * 
+ * La accion se envia como parametro en el metodo
  */
