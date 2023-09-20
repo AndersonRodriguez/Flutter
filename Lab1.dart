757db3b4d451db86106dce831daa670ff6dd3c27
AndersonRodriguez <anderson.rodri26@gmail.com>
2023-09-19 19:59:46 -0500

    Laboratorio

---
+++ Lab1.dart
@@ -0,0 +1,23 @@
+/**
+ * 
+ * Cajero automatico
+ * 
+ * Programa se inicia: Me va a pedir nombre y apellido
+ * 
+ * El usuario escribe nombre y apellido
+ * 
+ * Escoja una opcion: 
+ * 1. Retirar dinero
+ * 2. Ingresar dinero
+ * 3. Salir
+ * 
+ * Escojo la opcion y me va a pedir el monto
+ * 
+ * Ingrese el monto
+ * 
+ * La persona ingresa el monto y le muestra el total de su cuenta.
+ * 
+ * Anderson Rodriguez el total de su cuenta es: $1500
+ * 
+ * Tareita Mirar como capturar la informacion desde consola
+ */
