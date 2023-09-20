84641067638712c30b5aee5565d2a007e7e70d0d
AndersonRodriguez <anderson.rodri26@gmail.com>
2023-09-19 19:22:23 -0500

    Ejercicio 3

---
+++ ejercicio 3.dart
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Crear una clase base abstract Vehiculo
+ * 2 atributos: String tipo,int velocidadMaxima
+ * Contructor El que quieran
+ * Metodo: El vehiculo tipo ${tipo} acelera a una velocidad de ${velocidadMaxima} 
+ * 
+ * 
+ * Crear 2 objtos con herencia Automovil y motocicleta
+ * Atributos: cantidad de puertas, pasajeros
+ * 
+ * Agregar un nuevo metodo.
+ * 
+ * 
+ */
